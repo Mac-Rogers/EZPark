f0a34068eddf9971fd4d48f3489303fad6909030
Amber Chen <amberchen@Ambers-MacBook-Pro.local>
2024-07-02 10:54:25 +0800

    added searchbar and button console printing and changing footer

--- EZpark-Fullstack/frontend2/src/components/Footer.tsx
+++ EZpark-Fullstack/frontend2/src/components/Footer.tsx
@@ -1,11 +1,19 @@
-import React from "react";
+import React from 'react';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Footer: React.FC = () => {
-    return (
-        <div className="footer">
-          <button className="find-park">Find Me A Park</button>
-        </div>
-      );
-}
+  const handleClick = () => {
+    console.log('Find Me A Park button clicked');
+  };
+
+  return (
+    <div className="footer">
+      <button className="find-park" onClick={handleClick}>Find Me A Park</button>
+      <div className="location">
+        <FaLocationArrow />
+      </div>
+    </div>
+  );
+};
 
 export default Footer;
--- EZpark-Fullstack/frontend2/src/components/SearchBar.tsx
+++ EZpark-Fullstack/frontend2/src/components/SearchBar.tsx
@@ -1,22 +1,33 @@
-import React, { FC, ChangeEvent } from "react";
+import React, { useState, ChangeEvent, KeyboardEvent } from 'react';
 import { FaSearch } from "react-icons/fa";
 
-const SearchBar: FC = () => {
-    const handleInputChange = (event: ChangeEvent<HTMLInputElement>) => {
-        // Handle the input change if needed
-    };
+const SearchBar: React.FC = () => {
+  const [input, setInput] = useState<string>('');
 
-    return (
-        <div className="InputWrapper">
-            <FaSearch />
-            <input 
-                type="text"
-                placeholder="Type to find parking"
-                onChange={handleInputChange}
-                aria-label="Search for parking"
-            />
-        </div>
-    );
+  const handleChange = (event: ChangeEvent<HTMLInputElement>) => {
+    setInput(event.target.value);
+  };
+
+  const handleKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
+    if (event.key === 'Enter') {
+      console.log(input);
+      //setInput(''); //clears the search bar 
+    }
+  };
+
+  return (
+    <div className="input-wrapper">
+        <FaSearch />
+      <input
+        type="text"
+        placeholder="Type to search..."
+        value={input}
+        onChange={handleChange}
+        onKeyDown={handleKeyDown}
+      />
+    </div>
+  );
 };
 
 export default SearchBar;
+
