399a66b940ad0311d732b03337848acd1b9ec7a8
Kevin <114195310+Kevin-Gu-2022@users.noreply.github.com>
2024-07-02 11:02:02 +0800

    Integrate gps with backend

--- EZpark-Fullstack/backend/main.py
+++ EZpark-Fullstack/backend/main.py
@@ -4,6 +4,13 @@ from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 from . import models, crud
 import requests
+
+import threading
+import socket
+import ast
+from .location import Location
+import time
+
 from geopy.geocoders import Nominatim
 
 app = FastAPI()
@@ -20,6 +27,9 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# Lock used in multithreading
+lock = threading.Event()
 
 class ItemCreate(BaseModel):
     name: str
@@ -42,7 +52,7 @@ def create_item(item: ItemCreate, db: Session = Depends(get_db)):
 @app.get("/gps-coordinates")
 async def get_gps_coordinates():
     longitude, latitude = get_current_location()
-    return {"longitude": latitude, "latitude": longitude} 
+    return {"longitude": latitude, "latitude": longitude}
 
 # Finds network geolocation
 def get_public_ip():
@@ -59,3 +69,79 @@ def get_current_location():
     ip = get_public_ip()
     latitude, longitude = get_coordinates_from_ip(ip)
     return latitude, longitude
+
+
+def parse_location(location_raw: str) -> dict:
+    """
+    Many other useful fields returned by location data, which is why a dictionary is used.
+    Parse the location from received string and return the data that is most accurate in a dictionary
+    :param location_raw:
+    :return data: A dictionary with all fields returned by phone GPS
+    """
+    location_dict = ast.literal_eval(location_raw)  # Get dictionary object
+    # Determine the mode with the highest accuracy and use that
+    accuracy_list = {}
+    for mode in location_dict:
+        accuracy_list.update({mode: location_dict.get(mode).get('accuracy')})
+    data_mode = min(accuracy_list, key=accuracy_list.get)
+    data = location_dict.get(data_mode, {})
+    return data
+
+
+def trigger_request() -> None:
+    lock.set()
+
+
+def request_location(client: socket.socket):
+    while True:
+        lock.wait()
+        client.send("Location Request".encode())
+        packet = client.recv(2048)
+        location_raw = packet.decode('ascii')
+        if not location_raw:
+            print("Client closed connection")
+            break
+        data = parse_location(location_raw)
+
+        # Make sure it isn't an empty position
+        if data:
+            currentLocation = Location(data.get('latitude'),
+                                       data.get('longitude'),
+                                       data.get('accuracy'),
+                                       data.get('provider'))
+            print(currentLocation)
+
+            # Send this currentLocation somewhere to backend?
+
+        else:
+            print("No location data received")
+        lock.clear()
+
+
+def start_server():
+    s = socket.socket()
+    # Change this IP if needed
+    host = '192.168.185.84'
+    port = 12345
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    while True:
+        try:
+            s.bind((host, port))
+            break
+        except OSError:
+            print("Cannot connect to client. Consider using same network.")
+            time.sleep(10)
+    print("Started server")
+    s.listen(1)
+    client, addr = s.accept()
+    print(f"Accepted client at IP address {addr[0]} and port {addr[1]}")
+
+    request_location_thread = threading.Thread(target=request_location, args=(client,))
+    request_location_thread.start()
+
+
+
+# Start the server
+# start_server() # Maybe put on a thread
+
+
