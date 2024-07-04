1096fae04b998dc43b1f3a07bccc5921f89b6df2
Amber Chen <amberchen@Ambers-MacBook-Pro.local>
2024-07-04 10:57:42 +0800

    added EZ logo and attempted recenter button

--- EZpark-Fullstack/frontend2/src/components/LocationCentre.tsx
+++ EZpark-Fullstack/frontend2/src/components/LocationCentre.tsx
@@ -1,11 +1,33 @@
+// import React from "react";
+// import { FaLocationArrow } from "react-icons/fa";
+
+// const LocationCentre = () => {
+
+//     return (
+//         <div className="Location">
+//             <FaLocationArrow />
+//         </div>
+//     );
+// };
+
+// export default LocationCentre;
+
+
 import React from "react";
 import { FaLocationArrow } from "react-icons/fa";
 
-const LocationCentre = () => {
+const LocationCentre: React.FC = () => {
+
+    const handleClick = () => {
+        console.log("Icon clicked");
+        // Add your logic here
+    };
 
     return (
         <div className="Location">
-            <FaLocationArrow />
+            <button onClick={handleClick} style={{ background: 'none', border: 'none', cursor: 'pointer', padding: 0 }}>
+                <FaLocationArrow size={24} color="black" />
+            </button>
         </div>
     );
 };
